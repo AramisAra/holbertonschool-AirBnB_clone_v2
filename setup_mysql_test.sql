37331e4469852a03ee8035769bcef6a8f9284943
AramisAra <martinezcruzaj@gmail.com>
2024-03-29 11:37:46 -0400

    Adding fix of the my sql test file

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,16 @@
-_-- THIS QUERY SHOULD CREATE THE DATABASE AND USER AND GRANT PRIVILAGES
+-- Script that prepares a MySQL server for AirBnB Console v2
+-- This script will create a database and a user for the AirBnB project
+-- Create the database
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
-IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Create the user if not exists
+-- Create user in localhost identified by 'hbnb_test_pwd'
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant all privileges on the database to the user
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+Flush privileges;
+
+-- Grant select privileges on performance_schema to the user
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
+Flush privileges;
