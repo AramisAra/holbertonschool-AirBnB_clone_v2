d570cef5fd6cbefe410a5803703d172b502ff1e8
AramisAra <martinezcruzaj@gmail.com>
2024-04-10 14:50:27 -0400

    Adding thing to code

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Start a Flask web application.
+"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -12,4 +15,5 @@ def hello_hbnb():
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
